b4a187d9575ea20abcf6a5542e0a25ed26599622
leomdsousa <leonardomdsousa@gmail.com>
2023-05-03 01:03:35 -0300

    configs iniciais e criacao de algumaa acitivities vazias

---
+++ app/src/main/java/com/example/advogo/activities/PerfilActivity.kt
@@ -0,0 +1,12 @@
+package com.example.advogo.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.advogo.R
+
+class PerfilActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_perfil)
+    }
+}
--- app/src/main/java/com/example/advogo/activities/SplashScreenActivity.kt
+++ app/src/main/java/com/example/advogo/activities/SplashScreenActivity.kt
@@ -1,11 +1,12 @@
-package com.example.advogo
+package com.example.advogo.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.advogo.R
 
-class MainActivity : AppCompatActivity() {
+class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_splash_screen)
     }
 }
